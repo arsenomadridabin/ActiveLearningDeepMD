10b21cfd238d81b3cf0e25474afc162773577df1
Abin Shakya <arsenomadridabin@hotmail.com>
2024-09-24 09:42:02 -0500

    Removed unneccessary

--- temp_convert_script.py
+++
@@ -1,8 +0,0 @@
-from ase.io.vasp import write_vasp_xdatcar
-from ase.io.lammpsrun import read_lammps_dump_text
-
-fp = open("deepmd_3/lammps/out.dump", "r")
-read_data = read_lammps_dump_text(fp, index=slice(None))
-
-fp = open("XDATCAR", "w")
-write_vasp_xdatcar(fp, read_data)
